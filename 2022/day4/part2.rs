a362aecbb2cc7600d822bded23825a6a93f26e83
Maxim <44026362+ferzisdis@users.noreply.github.com>
2022-12-04 15:22:40 +0300

    Create part2.rs

---
+++ 2022/day4/part2.rs
@@ -0,0 +1,43 @@
+use std::{env, fs};
+use std::collections::hash_map::RandomState;
+use std::collections::HashSet;
+use std::fs::{File, OpenOptions};
+use std::io::{BufRead, BufReader, Read, Write};
+
+fn read_int<T>(it: &mut T) -> i32
+    where T : Iterator<Item = u8>
+{
+    let mut res = 0;
+    for b in it {
+        match b {
+            b'0'..=b'9' => {
+                res = res * 10 + i32::from(b - b'0')
+            }
+            _ => break
+        }
+    }
+
+    return  res;
+}
+
+fn main() {
+    println!("{}",  BufReader::new(File::open("day4.txt").expect("I know you are existed"))
+        .lines()
+        .map(|x| x.expect("I can do it!"))
+        .map(|s| {
+            let mut it = s.into_bytes().into_iter();
+            let l1 = read_int(&mut it);
+            let r1 = read_int(&mut it);
+            let l2 = read_int(&mut it);
+            let r2 = read_int(&mut it);
+
+            if l1 <= l2 && r2 <= r1
+                || l2 <= l1 && r1 <= r2
+                || l1 <= l2 && l2 <= r1
+                || l1 <= r2 && r2 <= r1 {
+                1
+            } else {
+                0
+            }
+        }).sum::<i32>());
+}
